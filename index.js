0d144ff22d4b310573f4dc0007e0a533098098f8
Efengsi Rahmanto Zalukhu <fengsifeng123@gmail.com>
2024-12-24 02:08:13 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// alert("Hello ini adalah Program Javascip");
+
+let name="Efengsi Rahmanto";
+let age=20;
+let merried=false;
+
+alert("Selamat datang " + name );
+document.write("Nama saya :" +name + "<br>");
+document.write("Umur saya :" +age + "<br>");
+document.write("Status Menikah saya :" + merried);
